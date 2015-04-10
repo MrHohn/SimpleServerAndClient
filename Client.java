614749ffb0e3cf949ee2e64db6d07c25329c399f
MrHohn <zzhonzi@gmail.com>
2015-04-10 12:45:23 -0400

    add judge command function

--- Client.java
+++ Client.java
@@ -4,15 +4,17 @@ import java.net.Socket;
 /**
  * Created by Hon on 4/7/2015.
  */
+
 public class Client {
     private static final int port_num = 5500;
     private static final String hostname = "localhost";
-    private Socket c_sock;
-    private BufferedReader in;
-    private PrintWriter out;
-    private BufferedReader userEntry;
-    private DataInputStream receiveFile;
-    private String serverResponse;
+    private Socket c_sock;               // socket
+    private BufferedReader in;           // input stream
+    private PrintWriter out;             // output stream
+    private BufferedReader userEntry;    // command line input
+    private DataInputStream receiveFile; // receive date stream
+    private String serverResponse;       // response from server
+    private String subCommand;           // detail follows command
 
 
     public Client() {
@@ -25,36 +27,77 @@ public class Client {
         }
         catch (IOException ex) {
             ex.printStackTrace();
-        }
-        
+        }  
     }
 
 
     public void run() {
         try {
             while (true) {
-                System.out.print("User, enter the command: ");
+                System.out.print("\nUser, enter the command: ");
                 String command = userEntry.readLine();
                 // judge whether the command is valid
                 command = judgeCommand(command);
 
-                // do corrosponding thinds according to the command
+                // do corresponding things according to the command
                 if (command.equals("EXIT")) {
-                    out.println(command);
+                    System.out.println("EXIT command.");
+                    exit(command);
                     break;
                 }
                 else if (command.equals("GET")) {
+                    System.out.println("GET command.");
                     get(command);
                     
                 }
                 else if (command.equals("BOUNCE")) {
-                    out.println(command);
+                    System.out.println("BOUNCE command.");
+                    bounce(command);
+                }
+                else {
+                    System.out.println("ERROR: Invalid command.");
                 }
             }
         }
         catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+
+
+    private String judgeCommand(String command) {
+        // if command is GET <***>    
+        if (command.length() > 6 && command.charAt(command.length() - 1) == '>' && command.substring(0, 5).equals("GET <")) {
+                    System.out.println("judge GET <>");
+                    subCommand = command.substring(5, command.length() - 1);
+                    System.out.println(subCommand);
+                    return "GET";
+        }
+        // if command is BOUNCE <***>
+        else if (command.length() > 9 && command.charAt(command.length() - 1) == '>' && command.substring(0, 8).equals("BOUNCE <")) {
+            System.out.println("judge BOUNCE <>");
+            subCommand = command.substring(8, command.length() - 1);
+            System.out.println(subCommand);
+            return "BOUNCE";
+        }
+        else if (command.length() >= 4 && command.substring(0, 4).equals("EXIT")) {
+            System.out.println("judge EXIT");
+            // if command is EXIT
+            if (command.length() == 4) {
+                subCommand = "none";
+                return command;
+            }
+            // if command is EXIT <***>
+            else if (command.length() > 7 && command.substring(3, 6).equals("T <") && command.charAt(command.length() - 1) == '>') {
+                    System.out.println("judge EXIT <>");
+                    subCommand = command.substring(6, command.length() - 1);
+                    System.out.println(subCommand);
+                    return "EXIT";
+            }
+        }
+
+        // If command invalid, return invalid
+        return "invalid";
     }
 
 
@@ -64,7 +107,7 @@ public class Client {
             serverResponse = in.readLine();
 
             if (serverResponse.equals("ok")) {
-                System.out.println("start receiving file...");
+                System.out.println("Start receiving file...");
                 // get file name and length
                 String fileName = receiveFile.readUTF();
                 long fileLength = receiveFile.readLong();
@@ -72,14 +115,15 @@ public class Client {
                 System.out.println("\nFile name: <" + fileName + ">");
                 System.out.println("Size: " + fileLength + " B");
                 System.out.println("--- FILE BEGIN ---\n");
+                // print out file content
                 while (receiveFile.available() != 0)
                 {
                     System.out.print((char) receiveFile.readByte());
                 }
-                System.out.println("\n\n--- FILE END ---\n");
+                System.out.println("\n\n--- FILE END --- ");
             }
             else {
-                System.out.println("File not exist.");
+                System.out.println("ERROR: File not exist.");
             }
         }
         catch (IOException ex) {
@@ -88,13 +132,16 @@ public class Client {
     }
 
 
-    private void bounce() {
-
+    private void bounce(String command) {
+        System.out.println("Still developing!");
+        out.println(command);   
     }
 
 
-    private void exit() {
+    private void exit(String command) {
         try {
+            out.println(command);
+            out.println(subCommand);
             receiveFile.close();
             c_sock.close();
         }
@@ -103,10 +150,6 @@ public class Client {
         }
     }
 
-    private String judgeCommand(String command) {
-
-        return command;
-    }
 
     public static void main(String[] args) {
         Client client = new Client();
